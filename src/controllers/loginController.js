1d35933e367440415a1cefa1c2e9dc461809952a
Diego <diegoramos3709@gmail.com>
2025-04-13 23:56:52 -0300

    Update loginController.js

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,64 +1,61 @@
-const Login = require('../models/loginModel');
+const Login = require("../models/loginModel");
 
-exports.index =(req,res) => {
-if(req.session.user) return res.render('login-logado');
-return res.render('login');
+exports.index = (req, res) => {
+  if (req.session.user) return res.render("login-logado");
+  return res.render("login");
 };
 
-exports.register = async function(req,res)  {
+exports.register = async function (req, res) {
+  try {
+    req.body.email = req.body.email.toLowerCase();
 
-    try{
-        const login = new Login(req.body);
-        await login.register();
-   
-        if(login.errors.length > 0 ){
-         req.flash('errors', login.errors);
-         req.session.save(function (){
-          return res.redirect('/login/index');
-         });
-         return;
-        }
-        req.flash('success', 'Seu usuario foi criado com sucesso.');
-        req.session.save(function (){
-         return res.redirect('/login/index');
+    const login = new Login(req.body);
+    await login.register();
 
-        
-        });
-        
-      }catch(e){
-        console.log(e);
-         return res.render('404');
-   }
-};
-  
-exports.login = async function(req,res)  {
-
-  try{
-      const login = new Login(req.body);
-      await login.login();
- 
-      if(login.errors.length > 0 ){
-       req.flash('errors', login.errors);
-       req.session.save(function (){
-        return res.redirect('/login/index');
-       });
-       return;
-      }
-
-      req.flash('success', 'Você entrou no sistema.'); 
-      req.session.user = login.user;
-      req.session.save(function (){
-       return res.redirect('/login/index');
+    if (login.errors.length > 0) {
+      req.flash("errors", login.errors);
+      req.session.save(function () {
+        return res.redirect("/login/index");
       });
-      }catch(e){
-      console.log(e);
-       return res.render('404');
- }
+      return;
+    }
+    req.flash("success", "Seu usuario foi criado com sucesso.");
+    req.session.save(function () {
+      return res.redirect("/login/index");
+    });
+  } catch (e) {
+    console.log(e);
+    return res.render("404");
+  }
 };
-exports.logout = function(req, res){
+
+exports.login = async function (req, res) {
+  try {
+    req.body.email = req.body.email.toLowerCase();
+
+    const login = new Login(req.body);
+    await login.login();
+
+    if (login.errors.length > 0) {
+      req.flash("errors", login.errors);
+      req.session.save(function () {
+        return res.redirect("/login/index");
+      });
+      return;
+    }
+
+    req.flash("success", "Você entrou no sistema.");
+    req.session.user = login.user;
+    req.session.save(function () {
+      return res.redirect("/login/index");
+    });
+  } catch (e) {
+    console.log(e);
+    return res.render("404");
+  }
+};
+exports.logout = function (req, res) {
   req.session.destroy((err) => {
-    res.redirect('/');
+    res.redirect("/");
   });
 };
-
-

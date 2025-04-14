c2b4ba8f61d420213c560bb91bbb7efadd6cc147
Diego <diegoramos3709@gmail.com>
2025-04-13 23:57:02 -0300

    Update contatoController.js

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -1,70 +1,70 @@
-const Contato = require('../models/contatoModel');
-
-
-exports.index = (req,res) =>{
-    res.render('contato',{
-        contato: {}
-    });
+const Contato = require("../models/contatoModel");
+const mongoose = require("mongoose");
+exports.index = (req, res) => {
+  res.render("contato", {
+    contato: {},
+  });
 };
-exports.register = async (req,res) =>{
-try{
+exports.register = async (req, res) => {
+  try {
+      req.body.user = req.session.user._id;
     const contato = new Contato(req.body);
-
     await contato.register();
-   
-   if(contato.errors.length > 0){
-       req.flash('errors', contato.errors);
-       req.session.save(() => res.redirect('/contato/index'));
-       return;
-   }
-   req.flash('success', 'Contato registrado com sucesso');
-   req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
-   return;
-}catch(e){
-    console.log(e);
-   return res.render('404');
-}
- 
-};
 
-exports.editIndex = async function(req,res){
-    if(!req.params.id) return res.render('404');
-
-    const contato = await Contato.buscaPorId(req.params.id);
-    if(!contato)return res.render('404');
-
-    res.render('contato',{contato});
-};
-
-exports.edit = async function(req,res){
-
-    try{
-        if(!req.params.id) return res.render('404');
-        const contato = new Contato(req.body);
-        await contato.edit(req.params.id);
-    
-        if(contato.errors.length > 0){
-            req.flash('errors', contato.errors);
-            req.session.save(() => res.redirect('/contato/index'));
-            return;
-        }
-        req.flash('success', 'Contato editado com sucesso');
-        req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
-        return;
-    }catch(e){
-        console.log(e);
-        res.render('404');
+    if (contato.errors.length > 0) {
+      req.flash("errors", contato.errors);
+      req.session.save(() => res.redirect("/contato/index"));
+      return;
     }
-  
+    req.flash("success", "Contato registrado com sucesso");
+    req.session.save(() =>
+      res.redirect(`/contato/index/${contato.Contato._id}`)
+    );
+    return;
+  } catch (e) {
+    console.log("Erro completo:", e);
+    return res.render("404");
+  }
 };
 
-exports.delete = async function(req,res){
-    if(!req.params.id) return res.render('404');
+exports.editIndex = async function (req, res) {
+  if (!req.params.id) return res.render("404");
 
-    const contato = await Contato.delete(req.params.id);
-    if(!contato)return res.render('404');
+  const contato = await Contato.buscaPorId(req.params.id);
+  if (!contato) return res.render("404");
 
-    req.flash('success', 'Contato apagado com sucesso');
-    req.session.save(() => res.redirect(`/`));
+  res.render("contato", { contato });
+};
+
+exports.edit = async function (req, res) {
+  try {
+    if (!req.params.id) return res.render("404");
+    const contato = new Contato(req.body);
+    await contato.edit(req.params.id);
+
+    if (contato.errors.length > 0) {
+      req.flash("errors", contato.errors);
+      req.session.save(() => res.redirect("/contato/index"));
+      return;
+    }
+    req.flash("success", "Contato editado com sucesso");
+    req.session.save(() =>
+      res.redirect(`/contato/index/${contato.contato._id}`)
+    );
     return;
+  } catch (e) {
+    console.log(e);
+    res.render("404");
+  }
+};
+
+exports.delete = async function (req, res) {
+  if (!req.params.id) return res.render("404");
+
+  const contato = await Contato.delete(req.params.id);
+  if (!contato) return res.render("404");
+
+  req.flash("success", "Contato apagado com sucesso");
+  req.session.save(() => res.redirect(`/`));
+  return;
 };
